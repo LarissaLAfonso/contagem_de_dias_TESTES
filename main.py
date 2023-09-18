ae366de6fce9209f50cfda151540f0bf251fa5c6
Matheus <matheuscarvalho210404@outlook.com>
2023-09-18 08:33:09 -0300

    Menu da Main

---
+++ main.py
@@ -0,0 +1,20 @@
+from contagem_dias import contagem_dias
+from leitor_de_arquivos import leitor_de_arquivos
+
+print("\nBem vindo ao contador_de_dias.py, siga as instruções a seguir.\n")
+
+while True:
+    print("Se você deseja inserir as datas manualmente, digite 1. \nSe você deseja inserir o caminho de um arquivo com as datas, digite 2.\n")
+    comando = input("Qual a sua escolha? ")
+
+    if comando == "1":
+        print("\nInsira as duas datas. Ex.: 28 de Agosto de 2023 - 18 de Setembro de 2023.")
+        datas = input("Insira aqui:")
+        break
+    elif comando == "2":
+        print("\nInsira o caminho do arquivo de texto. Ele deve conter apenas uma linha com as datas.")
+        caminho = input("Insira aqui: ")
+        datas = leitor_de_arquivos(caminho)
+        break
+    else:
+        print("\n--> Valor inválido. Insira 1 ou 2. <--\n")
