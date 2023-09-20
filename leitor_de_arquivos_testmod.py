a3904e50331aed826367f0f26f1d996c57cbc2d8
Larissa Lemos Afonso <126449248+LarissaLAfonso@users.noreply.github.com>
2023-09-20 09:07:53 -0300

    add docstring

--- leitor_de_arquivos_testmod.py
+++ leitor_de_arquivos_testmod.py
@@ -3,5 +3,6 @@ import unittest
 
 class TesteLeitor(unittest.TestCase):
     def verify_if_file_readable(self):
+        """Test file in which the inputed date is not in the 1st line"
         self.assertEqual(leitor_de_arquivos("data_testing.txt"), "12 de setembro de 2004 - 10 de agosto de 2021")
         self.assertEqual(leitor_de_arquivos("teste.txt"), "21 de Abril de 2022 - 30 de Setembro de 2005")
